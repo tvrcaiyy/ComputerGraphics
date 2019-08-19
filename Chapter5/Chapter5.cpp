af3c769cbb5d6749b776ccd524d434015e8ec1dd
tvrc <274510839@qq.com>
2019-08-20 06:12:19 +0800

    test

--- Chapter5/Chapter5.cpp
+++ Chapter5/Chapter5.cpp
@@ -6,22 +6,64 @@
 
 void init()
 {
-	glClearColor(0.0,0.0,1.0,1.0);
+	glClearColor(1.0,1.0,1.0,1.0);
 
-	glMatrixMode(GL_PROJECTION);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
-	gluOrtho2D(0.0,800,0.0,600);
+	//gluOrtho2D(0.0,800,0.0,600);
 }
 
 void displayFunc()
 {
-	glClear(GL_COLOR_BUFFER_BIT);
+	/*glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(0.0,1.0,0.0);
+	//glLineWidth(2);
+	glBegin(GL_POLYGON);
+	glVertex3i(100,500,0);
+	glVertex3i(100,100,0);
+	glVertex3i(500,100,0);
+
+	glVertex3i(500,100,0);
+	glVertex3i(500,500,10);
+	glVertex3i(100,500,0);
+	glEnd();
 
 	glColor3f(1.0,0.0,0.0);
-	glLineWidth(2);
-	glBegin(GL_LINE);
-	glVertex2i(100,100);
-	glVertex2i(500,500);
+	glPolygonMode(GL_FRONT,GL_LINE);
+	glBegin(GL_POLYGON);
+	glVertex3i(100,500,0);
+	glVertex3i(100,100,0);
+	glVertex3i(500,100,0);
+	
+	glVertex3i(500,100,0);
+	glVertex3i(500,500,10);
+	glVertex3i(100,500,0);
+	glEnd();*/
+
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.0,1.0,0.0);
+	//glLineWidth(2);
+	glBegin(GL_QUADS);
+	glVertex3f(-0.5,0.5,0);
+	glVertex3f(-0.5,-0.5,0);
+	glVertex3f(0.5,-0.5,0);
+
+	glVertex3f(0.5,-0.5,0);
+	glVertex3f(0.5,0.5,-1.0);
+	glVertex3f(-0.5,0.5,0);
+	glEnd();
+
+	glColor3f(1.0,0.0,0.0);
+	glPolygonMode(GL_FRONT,GL_LINE);
+	glBegin(GL_QUADS);
+	glVertex3f(-0.5,0.5,0);
+	glVertex3f(-0.5,-0.5,0);
+	glVertex3f(0.5,-0.5,0);
+
+	glVertex3f(0.5,-0.5,0);
+	glVertex3f(0.5,0.5,-1.0);
+	glVertex3f(-0.5,0.5,0);
 	glEnd();
 	glFlush();
 }
@@ -30,7 +72,7 @@ void reshapeFunc(int newWidth,int newHeight)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(0.0,newWidth,0.0,newHeight);
+	//gluOrtho2D(0.0,newWidth,0.0,newHeight);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
